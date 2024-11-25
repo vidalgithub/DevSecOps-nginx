38d2886c256f5219964046475724abe35c078912
vidalgithub <vidalngka@gmail.com>
2024-11-24 20:02:13 -0600

    created sbom-CICD.sh pipeline

---
+++ sbom-CICD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e # means the script will quit out if any of my command return a non-zero exit code 
+			
+APP_NAME=mywebapp
+VERSION=SECURE
+			
+#BUILD: Clenaup old container/build image
+docker rm -f $APP_NAME 2>/dev/null || true
+docker build -t $APP_NAME:$VERSION . 
+# Run docker scout and generate a file with all vulnerabilities in it. We then set up Jenkins (or Github Actions) to send it through email or as a slack attachment in your pipeline.
+docker scout cves $APP_NAME:$VERSION --output ./vulns.report  
+# Run docker scout again and set a treshhold
+docker scout cves $APP_NAME:$VERSION --only-severity critical --exit-code
+# Generate SBOM file
+docker scout sbom --output mywebapp.sbom mywebapp:SECURE
+			
+#TEST: Run the container
+docker run -d -p 8182:80 --name webapp $APP_NAME:$VERSION
